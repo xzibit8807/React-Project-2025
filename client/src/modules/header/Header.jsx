45f9af29aff39ce41167af80032c77a146e79025
Svetlomir Delsizov <freeway88@abv.bg>
2025-03-17 09:43:40 +0200

    FIxes on the CSS and starting the Router
    there will be more fixes on the CSS because I dont like the vision now. Work in Progress.
    Now first thing is to make the React Router and to aplay the routes.

--- client/src/modules/header/Header.jsx
+++ client/src/modules/header/Header.jsx
@@ -1,43 +1,77 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react"; // Icons for the toggle button
+
+
 export default function HeaderComp() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className="header_section">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="logo"><a href="/"><img src="images/logo.png"/></a></div>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+        <header className="nav">
+            <div className="header_section">
+
+                {/* Desktop Navigation */}
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="logo"><a href="/"><img src="images/logo.png" /></a></div>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a className="nav-a" href="/">HOME</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-a" href="/about">ABOUT</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-a" href="/games">VIDEO GAMES</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-a" href="/blog">BLOG</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-a" href="/contact">CONTACT US</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-a" href="/"><img src="images/search-icon.png" /></a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-a" href="/login">SIGN IN</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-a" href="/register">REGISTER</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-a" href="/logout">LOG OUT</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+
+                {/* Mobile Menu Button */}
+                <button
+                    className="md:hidden"
+                    onClick={() => setIsOpen(!isOpen)}
+                    aria-label="Toggle mobile menu"
+                >
+                    {isOpen ? <X size={24} /> : <Menu size={24} />}
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <a className="nav-a" href="/">HOME</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-a" href="/about">ABOUT</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-a" href="/product">OUR PRODUCTS</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-a" href="/games">VIDEO GAMES</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-a" href="/blog">BLOG</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-a" href="/contact">CONTACT US</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-a" href="/"><img src="images/search-icon.png"/></a>
-                        </li>
-                        <li className="nav-item active">
-                            <a className="nav-a" href="/login">SIGN IN</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-a" href="/register">REGISTER</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+            </div>
+
+            {/* Dropdown Menu (Only Visible on Small Screens) */}
+            {isOpen && (
+                <nav className="md:hidden bg-blue-700 p-2">
+                    <a href="/" className="block py-2 px-4 hover:bg-blue-500">HOME</a>
+                    <a href="/about" className="block py-2 px-4 hover:bg-blue-500">ABOUT</a>
+                    <a href="/games" className="block py-2 px-4 hover:bg-blue-500">VIDEO GAMES</a>
+                    <a href="/contact" className="block py-2 px-4 hover:bg-blue-500">CONTACT US</a>
+                    <a href="/blog" className="block py-2 px-4 hover:bg-blue-500">BLOG</a>
+                    <a href="/signin" className="block py-2 px-4 hover:bg-blue-500">SIGN IN</a>
+                    <a href="/register" className="block py-2 px-4 hover:bg-blue-500">REGISTER</a>
+                    <a href="/logout" className="block py-2 px-4 hover:bg-blue-500">LOGOUT</a>
+                </nav>
+            )}
+        </header>
     );
 }
+
+
