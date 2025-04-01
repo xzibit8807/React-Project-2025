1180fef76ca0c4a1a62b8919bd25df9bd9b3d179
Svetlomir Delsizov <freeway88@abv.bg>
2025-04-01 10:42:08 +0300

    fixed a css bug so that 404 displays under the header

--- client/src/modules/noPage/NoPage404.jsx
+++ client/src/modules/noPage/NoPage404.jsx
@@ -1,16 +1,17 @@
 import "./noPage.css"
 export default function NoPageTemp() {
     return (
-        <main className="bsod container404">
+        <div className="bsod container404">
             <h1 className="neg title"><span className="bg">Error - 404</span></h1>
             <p>An error has occurred, to continue:</p>
             <p>* You chose the wrong way.</p>
             <p>* Return to our homepage.<br />
                 * For some reason here is nothing to see.</p>
             <p>* Please next time do stick on the right road, so you don't get lost.</p>
-            <nav className="nav">
+            <p>* From here on you can return back to:</p>
+            <div className="back">
                 <a href="/" className="link">Home Page</a>
-            </nav>
-        </main>
+            </div>
+        </div>
     );
 }
