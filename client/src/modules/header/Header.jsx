7f12228a03531904e1bcc4acb9777da62b4ddad9
Svetlomir Delsizov <freeway88@abv.bg>
2025-04-02 20:31:10 +0300

    fixing again the header CSS

--- client/src/modules/header/Header.jsx
+++ client/src/modules/header/Header.jsx
@@ -25,26 +25,23 @@ export default function HeaderComp() {
                         {isOpen ? <X size={24} /> : <Menu size={24} />}
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <div className="navbar-nav mr-auto">
-
+                        <div className="navbar-nav">
                             <Link to="/" className="logo"><img src="images/logo2.png" /></Link>
-                            {isAuthenticated
-                                ? (<div>
-                                    <Link to="/games/create" className="nav-item">ADD GAME</Link>
-                                </div>) : (<div />)}
+                            {isAuthenticated && (
+                            <Link to="/games/create" className="nav-item">ADD GAME</Link>
+                        )}
                             <Link to="/about" className="nav-item">ABOUT</Link>
                             <Link to="/products" className="nav-item">VIDEO GAMES</Link>
                             <Link to="/contact" className="nav-item">CONTACT US</Link>
-                            {isAuthenticated
-                                ? (<div>
-                                    <Link to="/blog" className="nav-item">BLOG</Link>
-                                </div>) : (<div />)}
+                            {isAuthenticated && (
+                            <Link to="/blog" className="nav-item">BLOG</Link>
+                        )}
                             <Link to="/search" className="nav-item"><img src="images/search-icon.png" />Search</Link>
                             {isAuthenticated
                                 ? (
                                     <div className="collapse navbar-collapse" id="navbarSupportedContent">                                       
                                         <Link to="/profile" className="nav-item">PROFILE</Link>
-                                        <Link to="/logout" className="nav-item">LOG OUT</Link>
+                                        <Link to="/logout" className="navbar-nav mr-auto">LOG OUT</Link>
                                         {email}
                                     </div>
                                 )
@@ -55,8 +52,6 @@ export default function HeaderComp() {
                                     </div>
                                 )
                             }
-
-
                         </div>
                     </div>
                 </nav>
@@ -66,13 +61,20 @@ export default function HeaderComp() {
             {isOpen && (
                 <nav className="md:hidden bg-blue-700 p-2">
                     <Link to="/" className="block py-2 px-4 hover:bg-blue-500">HOME</Link>
+                    {isAuthenticated && (
+                            <Link to="/games/create" className="block py-2 px-4 hover:bg-blue-500">ADD GAME</Link>
+                        )}
                     <Link to="/about" className="block py-2 px-4 hover:bg-blue-500">ABOUT</Link>
                     <Link to="/products" className="block py-2 px-4 hover:bg-blue-500">VIDEO GAMES</Link>
                     <Link to="/contact" className="block py-2 px-4 hover:bg-blue-500">CONTACT US</Link>
+                    {isAuthenticated && (
+                            <Link to="/blog" className="block py-2 px-4 hover:bg-blue-500">BLOG</Link>
+                        )}
+                        <Link to="/search" className="block py-2 px-4 hover:bg-blue-500">Search</Link>
                     {isAuthenticated
                         ? (
                             <div>
-                                <Link to="/blog" className="block py-2 px-4 hover:bg-blue-500">BLOG</Link>
+                                <Link to="/profile" className="block py-2 px-4 hover:bg-blue-500">PROFILE</Link>
                                 <Link to="/logout" className="block py-2 px-4 hover:bg-blue-500">LOGOUT</Link>
                             </div>
                         )
