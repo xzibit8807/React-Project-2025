de3c6673953aca5edb82781e661946787c88d696
Svetlomir Delsizov <freeway88@abv.bg>
2025-04-02 20:32:51 +0300

    fixed the lines

--- client/src/modules/header/Header.jsx
+++ client/src/modules/header/Header.jsx
@@ -28,18 +28,18 @@ export default function HeaderComp() {
                         <div className="navbar-nav">
                             <Link to="/" className="logo"><img src="images/logo2.png" /></Link>
                             {isAuthenticated && (
-                            <Link to="/games/create" className="nav-item">ADD GAME</Link>
-                        )}
+                                <Link to="/games/create" className="nav-item">ADD GAME</Link>
+                            )}
                             <Link to="/about" className="nav-item">ABOUT</Link>
                             <Link to="/products" className="nav-item">VIDEO GAMES</Link>
                             <Link to="/contact" className="nav-item">CONTACT US</Link>
                             {isAuthenticated && (
-                            <Link to="/blog" className="nav-item">BLOG</Link>
-                        )}
+                                <Link to="/blog" className="nav-item">BLOG</Link>
+                            )}
                             <Link to="/search" className="nav-item"><img src="images/search-icon.png" />Search</Link>
                             {isAuthenticated
                                 ? (
-                                    <div className="collapse navbar-collapse" id="navbarSupportedContent">                                       
+                                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
                                         <Link to="/profile" className="nav-item">PROFILE</Link>
                                         <Link to="/logout" className="navbar-nav mr-auto">LOG OUT</Link>
                                         {email}
@@ -62,15 +62,15 @@ export default function HeaderComp() {
                 <nav className="md:hidden bg-blue-700 p-2">
                     <Link to="/" className="block py-2 px-4 hover:bg-blue-500">HOME</Link>
                     {isAuthenticated && (
-                            <Link to="/games/create" className="block py-2 px-4 hover:bg-blue-500">ADD GAME</Link>
-                        )}
+                        <Link to="/games/create" className="block py-2 px-4 hover:bg-blue-500">ADD GAME</Link>
+                    )}
                     <Link to="/about" className="block py-2 px-4 hover:bg-blue-500">ABOUT</Link>
                     <Link to="/products" className="block py-2 px-4 hover:bg-blue-500">VIDEO GAMES</Link>
                     <Link to="/contact" className="block py-2 px-4 hover:bg-blue-500">CONTACT US</Link>
                     {isAuthenticated && (
-                            <Link to="/blog" className="block py-2 px-4 hover:bg-blue-500">BLOG</Link>
-                        )}
-                        <Link to="/search" className="block py-2 px-4 hover:bg-blue-500">Search</Link>
+                        <Link to="/blog" className="block py-2 px-4 hover:bg-blue-500">BLOG</Link>
+                    )}
+                    <Link to="/search" className="block py-2 px-4 hover:bg-blue-500">Search</Link>
                     {isAuthenticated
                         ? (
                             <div>
