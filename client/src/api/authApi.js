f36a737bb5f4f93d57f0aa3d63a094bf3be85a2d
Svetlomir Delsizov <freeway88@abv.bg>
2025-04-24 11:13:37 +0300

    making the Auth Api work withe the new made by me server
    Still Work in Progress...

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -4,42 +4,44 @@ import request from "../utils/request";
 import { UserContext } from "../hooks/context";
 
 // const baseUrl = `${import.meta.env.VITE_APP_SERVER_URL}/users`;
-const baseUrl = 'http://localhost:3030/users';
-
-// export const useLogin = () => {
-//     const login = async (email, password) =>
-//         await request.post(
-//             `${baseUrl}/login`,
-//             { email, password },
-//         );
-//     if (login.success) {
-//         localStorage.setItem('accessToken', response.data.accessToken);
-//     }
-//         return { login }
-// };
+const baseUrl = 'http://localhost:3030/users/';
 
 export const useLogin = () => {
-    const login = async (email, password) => {
-        const response = await request.post(`${baseUrl}/login`, { email, password });
-        const { accessToken, user } = response.data;
+    const login = async (email, password) =>
+        request.post(
+            `${baseUrl}/login`,
+            { email, password },
+        );
 
-        // ✅ Fix: Ensure response has an accessToken before storing it
-        if (response.accessToken) {
-            // localStorage.setItem('accessToken', response.accessToken);
-            localStorage.setItem('accessToken', accessToken);
-            localStorage.setItem('userId', user._id);
-            localStorage.setItem('email', user.email);
-        }
-        setUserData({
-            accessToken,
-            _id: user._id,
-            email: user.email,
-        });
-        return response;
-    };
-
-    return { login };
+    return {
+        login,
+    }
 };
+
+// export const useLogin = () => {
+//     const login = async (email, password) => {
+//         const response = await request.post(`${baseUrl}/login`, { email, password });
+//         const { accessToken, user } = response.data;
+
+//         // ✅ Fix: Ensure response has an accessToken before storing it
+//         if (response.accessToken) {
+//             // localStorage.setItem('accessToken', response.accessToken);
+//             localStorage.setItem('accessToken', accessToken);
+//             localStorage.setItem('userId', user._id);
+//             localStorage.setItem('email', user.email);
+//         }
+//         setUserData({
+//             accessToken,
+//             _id: user._id,
+//             email: user.email,
+//         });
+//         return response;
+//     };
+
+//     return { login };
+// };
+
+
 
 export const useRegister = () => {
     const register = (email, password) =>
