bdfc5eb38840def81b48bb70aa9db266da82d726
Svetlomir Delsizov <freeway88@abv.bg>
2025-08-02 22:02:06 +0300

    testing the mongo DB Online
    making some changeses to the route link for mongoDB

--- server/server.js
+++ server/server.js
@@ -5,18 +5,20 @@ const cors = require('cors');
 
 const authRoutes = require('./routes/auth');
 const gameRoutes = require('./routes/games');
+// const router = require('./router/router');
 
 const app = express();
 const port = 3030;
 app.use(cors());
 app.use(express.json());
 
+// app.use(router)
 app.use('/users', authRoutes);
 app.use('/games', gameRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        app.listen(port, () => console.log(`Server running on  port ${port}\n => This is the way.`));
+        app.listen(port, () => console.log(`DataBase is up and running on  port ${port}\n => This is the way.`));
     })
     .catch(err => console.error(`Faller connecting to database:`, err.message));
 
