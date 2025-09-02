652caacd918048bcfccefa75ae2036afc56f527e
Svetlomir Delsizov <freeway88@abv.bg>
2025-09-03 00:41:06 +0300

    server and host deployed

--- backend/router/router.js
+++ backend/router/router.js
@@ -2,6 +2,14 @@ const router = require('express').Router();
 const authRoutes = require('../routes/auth');
 const gamesRoutes = require('../routes/games');
 
+router.get("/", (req, res) => {
+    res.status(200).send("✅ API is running!");
+});
+
+router.get("/status", (req, res) => {
+    res.status(200).json({ status: "ok", uptime: process.uptime() });
+});
+
 router.use('/users', authRoutes);
 router.use('/games', gamesRoutes);
 
