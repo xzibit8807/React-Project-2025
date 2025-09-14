8129945998d1a67a51d4e3bde256d65a89332b9e
Svetlomir Delsizov <freeway88@abv.bg>
2025-09-14 22:30:41 +0300

    Update Comments.js

--- backend/models/Comments.js
+++ backend/models/Comments.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema(
     {
-        user: { type: String, required: true },   // по-късно може да е userId вместо име
+        user: { type: String, required: true },   
         comment: { type: String, required: true },
     },
     { timestamps: true }
